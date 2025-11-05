47f27122de6ea00e7aa6bad6e78751245ab30187
adminzulfa <zulfarida0107@gmail.com>
2025-11-05 10:10:11 +0700

    fix: update judul halaman login menjadi 'Log in admin'

--- login copy.php
+++ login copy.php
@@ -10,7 +10,7 @@ if (isset($_SESSION['user'])) {
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <title>Admin Panel | Log in</title>
+  <title>Log in admin</title>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
