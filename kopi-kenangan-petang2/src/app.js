5130e729574f008cb15de161ca5c9f2dc45846f3
namira <namiraassalwa@gmail.com>
2025-11-25 18:12:14 +0700

    Update file app.js ubah harga aceh gayo 35000 jadi 34000

--- kopi-kenangan-petang2/src/app.js
+++ kopi-kenangan-petang2/src/app.js
@@ -4,7 +4,7 @@ document.addEventListener("alpine:init", () => {
       { id: 1, name: "Robusta Brazil", img: "1.jpg", price: 20000 },
       { id: 2, name: "Arabika Blend", img: "2.jpg", price: 25000 },
       { id: 3, name: "Primo Passo", img: "3.jpg", price: 30000 },
-      { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 35000 },
+      { id: 4, name: "Aceh Gayo", img: "4.jpg", price: 34000 },
       { id: 5, name: "Sumatra Mandheling", img: "5.jpg", price: 40000 },
     ],
   }));
